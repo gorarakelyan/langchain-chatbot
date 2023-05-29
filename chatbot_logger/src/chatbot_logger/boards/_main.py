cfc4810856dc3817ccbdfb04163c45e8bf203763
Gor Arakelyan <arakelyangor10@gmail.com>
2023-05-28 18:50:38 -0700

    Adjust logger boards overview

--- chatbot_logger/src/chatbot_logger/boards/_main.py
+++ chatbot_logger/src/chatbot_logger/boards/_main.py
@@ -1,13 +1,13 @@
 ui.header('ChatBot Logger')
 
-ui.subheader('Production Monitoring')
+ui.subheader('Production')
 ui.board_link('production.py', 'Production Overview')
 ui.board_link('analytics.py', 'User Page')
+
+ui.subheader('Chat')
+ui.board_link('sessions.py', 'Individual Session')
 
 ui.subheader('Development')
 ui.board_link('development/_main.py', 'Development Overview')
 ui.board_link('development/experiment.py', 'Experiments')
 ui.board_link('development/release.py', 'Releases')
-
-ui.subheader('Chat')
-ui.board_link('sessions.py', 'Individual Session')
